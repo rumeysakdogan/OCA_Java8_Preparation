651bddc87eff3323fda1d1f6b85f31a11b6acd18
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-13 12:35:02 -0700

    Question 2

---
+++ src/Question2.java
@@ -0,0 +1,13 @@
+public class Question2 {
+
+    public static void main(String[] args) {
+
+        //Q: What is the result?
+
+        int i = 10;
+        int j = 20;
+        int k = (j += i)/ 5;   // k = ( 20+10)/5  --> k=6
+        System.out.print(i + " : " + j + " : " + k);  // 10 : 30 : 6
+
+    }
+}
