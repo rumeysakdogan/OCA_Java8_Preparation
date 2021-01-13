5564de6a104282fbed204c45f0b330e25ad6c6ce
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-13 12:52:14 -0700

    Question 4

---
+++ src/Question4.java
@@ -0,0 +1,26 @@
+public class Question4 {
+
+    public static void main(String[] args) {
+
+        // Q: Which code fragment, inserted at line n1, prints The Top element: 30?
+
+        int[] stack = {10,20,30};
+        int size = 3;
+        int idx = 0;
+        /* line n1  */
+        do{                            // 1 < 2
+            idx++;                      // 2 < 2 false exit the loop idx = 2
+        }while( idx < size-1);
+        System.out.print("The Top element: " + stack[idx]);
+        // To print 30 --> idx = 2
+
+        /*
+        Solution:
+         do{                            // 1 < 2
+            idx++;                      // 2 < 2 false exit the loop idx = 2
+          }while( idx < size-1);
+
+         */
+
+    }
+}
