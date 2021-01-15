c3dda5603be0ddfca410945802f3e6891f291706
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-15 14:07:04 -0700

    Question 30 is added

---
+++ src/q_30/Q030.java
@@ -0,0 +1,26 @@
+package q_30;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q030 {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Robb");
+        names.add("Bran");
+        names.add("Rick");
+        names.add("Bran");
+
+        if (names.remove("Bran")) {
+            names.remove("Jon");
+        }
+        System.out.println(names);
+    }
+}
+//What is the result?
+//A.	[Robb, Rick, Bran]
+//B.	[Robb, Rick]
+//C.	[Robb, Bran, Rick, Bran]
+//D.	An exception is thrown at runtime.
+
