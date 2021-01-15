e1ea4936adc5905c0f440d03457a2be22685d4de
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-15 11:36:58 -0700

    Question 6

---
+++ src/Question5.java
@@ -0,0 +1,11 @@
+public class Question5 {
+
+    /*
+    5. Which statement is true about the switch statement?
+A. It must contain the default section.
+B. The break statement, at the end of each case block, is optional.
+ C. Its case label literals can be changed at runtime.
+D. Its expression must evaluate to a collection of values.
+Answer: B
+     */
+}
