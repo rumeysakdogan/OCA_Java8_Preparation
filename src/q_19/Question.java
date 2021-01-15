7824633fd70cc0054d7756e019a81e4f7be9e515
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-15 13:25:45 -0700

    Question 19 added

---
+++ src/q_19/Question.java
@@ -0,0 +1,15 @@
+package q_19;
+
+public class Question {
+
+    /*
+    Which three statements are true about the structure of a Java class? (Choose three.)
+        A. A class cannot have the same name as its field.
+        B. A public class must have a main method.
+        C. A class can have final static methods.
+        D. A class can have overloaded private constructors.
+        E. Fields need to be initialized before use.
+        F. Methods and fields are optional components of a class. Answer: B D E
+     */
+
+}
